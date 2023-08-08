27ef06174cc182ae36a676664e9121624629c510
Gabriel Pagotto <gabriel.pagotto@icloud.com>
2023-08-08 20:04:43 -0300

    Update posts.module.ts

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { StorageService } from '../storage/storage.service';
 
 @Module({
     controllers: [PostsController],
-    providers: [PostsService, PrismaService],
+    providers: [PostsService, PrismaService, StorageService],
 })
 export class PostsModule { }
