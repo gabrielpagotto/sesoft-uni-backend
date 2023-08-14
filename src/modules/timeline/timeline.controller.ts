93dde30a493a18baabf715f3cdd9d019741a38fe
Gabriel Pagotto <gabriel.pagotto@icloud.com>
2023-08-14 09:49:44 -0300

    Improvements: update timeline posts

--- src/modules/timeline/timeline.controller.ts
+++ src/modules/timeline/timeline.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { TimelineService } from './timeline.service';
 import { CurrentUser } from 'src/decorators/current-user.decorator';
 import { User } from '@prisma/client';
@@ -10,7 +10,7 @@ export class TimelineController {
     constructor(private readonly timelineService: TimelineService) { }
 
     @Get()
-    timeline(@CurrentUser() currentUser: User) {
-        return this.timelineService.timeline(currentUser);
+    timeline(@CurrentUser() currentUser: User, @Query('skip') skip?: number, @Query('take') take?: number) {
+        return this.timelineService.timeline(currentUser, skip, take);
     }
 }
--- src/modules/timeline/timeline.service.ts
+++ src/modules/timeline/timeline.service.ts
@@ -2,25 +2,47 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
 import { ApiResponse } from '@nestjs/swagger';
+import { DEFAULT_QUERY_SKIP, DEFAULT_QUERY_TAKE } from 'src/constants/query.constant';
 
 @Injectable()
 export class TimelineService {
     constructor(private db: PrismaService) { }
 
     @ApiResponse({ status: HttpStatus.OK, description: 'Realiza a montagem da timeline do usuário atual.' })
-    async timeline(currentUser: User) {
-        return await this.db.$queryRaw`
-          SELECT P.* FROM posts P
-            JOIN follows f ON P.user_id = f.user_following_id
-            WHERE f.user_followed_id = ${currentUser.id}
-            AND p.created_at >= (
-                    SELECT f2.created_at
-                    FROM follows f2
-                    WHERE f2.user_followed_id = ${currentUser.id}
-                    AND f2.user_following_id = p.user_id
-            )
-            AND P.post_id IS NULL
-            ORDER BY P.created_at DESC;
-        `
+    async timeline(currentUser: User, skip?: number, take?: number) {
+        const whereCondition = {
+            AND: [
+                {
+                    user: { followed: { every: { userFollowingId: currentUser.id } } }
+                },
+                {
+                    postId: null,
+                },
+            ],
+        };
+        const timelinePosts = await this.db.post.findMany({
+            orderBy: { createdAt: 'desc' },
+            where: whereCondition,
+            skip: !skip ? DEFAULT_QUERY_SKIP : Number(skip),
+            take: !take ? DEFAULT_QUERY_TAKE : Number(take),
+            select: {
+                id: true,
+                content: true,
+                createdAt: true,
+                updatedAt: true,
+                likesCount: true,
+                user: {
+                    select: {
+                        id: true,
+                        email: true,
+                        username: true,
+                        profile: { select: { displayName: true, bio: true, icon: true } }
+                    },
+                }
+            },
+
+        });
+        const count = await this.db.post.count({ where: whereCondition });
+        return { count, result: timelinePosts }
     }
 }
