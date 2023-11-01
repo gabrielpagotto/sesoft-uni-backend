fb7a559a348e418930e2284c860e439f77afaf87
Gabriel Pagotto <gabriel.pagotto@icloud.com>
2023-10-31 22:02:05 -0300

    Fix: environment not in the right format

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -2,7 +2,7 @@ export const serviceAccount = {
     type: process.env.FIREBASE_ADMIN_TYPE,
     project_id: process.env.FIREBASE_ADMIN_ID,
     private_key_id: process.env.FIREBASE_ADMIN_KEY_ID,
-    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
     auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
