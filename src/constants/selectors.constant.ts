02662eeabdf0685b35148925113e1226cdffc28c
Gabriel Pagotto <gabriel.pagotto@icloud.com>
2023-11-22 22:59:22 -0300

    fix: posts in timeline

---
+++ src/constants/selectors.constant.ts
@@ -0,0 +1,22 @@
+export const listPostSelector = {
+    id: true,
+    content: true,
+    createdAt: true,
+    updatedAt: true,
+    likesCount: true,
+    repliesCount: true,
+    user: {
+        select: {
+            id: true,
+            email: true,
+            username: true,
+            profile: {
+                select: {
+                    displayName: true,
+                    bio: true,
+                    icon: true,
+                },
+            },
+        },
+    },
+};
--- src/modules/timeline/timeline.service.ts
+++ src/modules/timeline/timeline.service.ts
@@ -4,16 +4,17 @@ import {
     DEFAULT_QUERY_SKIP,
     DEFAULT_QUERY_TAKE,
 } from 'src/constants/query.constant';
+import { listPostSelector } from 'src/constants/selectors.constant';
 import { PaginatedResponse } from 'src/types/paginated-response.type';
-import { PrismaService } from '../prisma/prisma.service';
 import { PostsService } from '../posts/posts.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class TimelineService {
     constructor(
         private readonly db: PrismaService,
         private readonly postService: PostsService,
-    ) {}
+    ) { }
 
     async timeline(currentUser: User, skip?: number, take?: number) {
         const whereCondition = {
@@ -21,7 +22,7 @@ export class TimelineService {
                 {
                     user: {
                         followed: {
-                            every: { userFollowingId: currentUser.id },
+                            some: { userFollowingId: currentUser.id },
                         },
                     },
                 },
@@ -32,31 +33,23 @@ export class TimelineService {
         };
         const timelinePosts = await this.db.post.findMany({
             orderBy: { createdAt: 'desc' },
-            where: whereCondition,
-            skip: !skip ? DEFAULT_QUERY_SKIP : Number(skip),
-            take: !take ? DEFAULT_QUERY_TAKE : Number(take),
-            select: {
-                id: true,
-                content: true,
-                createdAt: true,
-                updatedAt: true,
-                likesCount: true,
-                repliesCount: true,
-                user: {
-                    select: {
-                        id: true,
-                        email: true,
-                        username: true,
-                        profile: {
-                            select: {
-                                displayName: true,
-                                bio: true,
-                                icon: true,
+            where: {
+                AND: [
+                    {
+                        user: {
+                            followed: {
+                                some: { userFollowingId: currentUser.id },
                             },
                         },
                     },
-                },
+                    {
+                        postId: null,
+                    },
+                ],
             },
+            skip: !skip ? DEFAULT_QUERY_SKIP : Number(skip),
+            take: !take ? DEFAULT_QUERY_TAKE : Number(take),
+            select: listPostSelector,
         });
 
         // @TODO Refatorar.
@@ -67,12 +60,10 @@ export class TimelineService {
                 post.id,
             );
             const files = await this.postService.findFilesPost(post.id);
-
             posts.push({ ...post, liked, files });
         }
 
         const count = await this.db.post.count({ where: whereCondition });
-
         return { count, result: posts } satisfies PaginatedResponse<
             typeof posts
         >;
